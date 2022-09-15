5ea86e40764a002cf55bb30dca52bace3eaef786
Colin Sasser <p23csasser@fairfieldprep.org>
2022-09-15 10:48:21 -0400

    Submission day 2 cycle 2

---
+++ lab_2-3.py
@@ -0,0 +1,13 @@
+#Author CS 9/15/2022
+a=2
+print (a)
+print (type(a))
+a = float(a)
+print(a)
+print (type(a))
+a = str(a)
+print (type(a))
+a = bool(a)
+print (type(a))
+print (a == 2) 
+#It is false because after changing a == 2 to a float it changes to 2.0 which is different than 2 
