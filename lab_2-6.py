e1ec287d3995c76eaca6a1c7244be7d1ab8a7786
Colin Sasser <p23csasser@fairfieldprep.org>
2022-09-20 13:43:44 -0400

    2.6

---
+++ lab_2-6.py
@@ -0,0 +1,27 @@
+# Creator CS 9/20/2022
+
+a = 5
+b = 10
+c = 0
+d = -4
+
+if a % 2 == 0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
+if b % 2 == 0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
+if c % 2 == 0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
+if d % 2 == 0:
+    print('Number is even')
+else:
+    print('Number is odd')
+
